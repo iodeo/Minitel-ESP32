bb23a5da492f28a9111acdeb2fd59bc9528bb764
iodeo <louis.henrionnet@gmail.com>
2021-11-18 14:48:35 +0100

    Update main.py

--- upython/upynitel/main.py
+++ upython/upynitel/main.py
@@ -4,3 +4,7 @@ from machine import UART
 import upynitel
 
 m = upynitel.Pynitel(UART(2, baudrate = 1200,parity=0, bits=7,stop=1))
+print(' > minitel instance declared')
+
+m._print('hello world')
+print(' > print hello world')
