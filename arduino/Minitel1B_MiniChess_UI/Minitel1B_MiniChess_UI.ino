3b7c23f08391057a686754b54a4545b311f0db38
iodeo <contact@iodeo.fr>
2022-03-30 04:03:13 +0200

    rearrangement using MiniChess lib

---
+++ arduino/Minitel1B_MiniChess_UI/Minitel1B_MiniChess_UI.ino
@@ -0,0 +1,26 @@
+#include "Minichess.h"
+#include <Minitel1B_Hard.h>
+
+#define MINITEL_PORT Serial2 // For ESP32 dev board
+//#define MINITEL_PORT Serial1 // For ATMEGA32u4
+#define BAUDS 4800
+
+MiniChess chess(MINITEL_PORT);
+
+void setup() {
+  // réglage port série et config minitel
+  chess.initializeMinitel(BAUDS); 
+
+  // initialisation du jeu
+  chess.drawChessBoard();     // affichage plateau vide
+  chess.initPiecesPosition(); // placement des pieces
+  chess.drawAllPieces();      // affichage des pièces
+}
+
+void loop() {
+  while (true) {
+    chess.getMove();
+    chess.makeMove();
+    chess.changePlayer();
+  }
+}
