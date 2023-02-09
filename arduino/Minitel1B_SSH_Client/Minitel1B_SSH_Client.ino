dc60903a221b45a667f2380b95668c1548eecfdd
iodeo <contact@iodeo.fr>
2023-02-09 14:42:20 +0100

    Use freertos task to increase stack size

--- arduino/Minitel1B_SSH_Client/Minitel1B_SSH_Client.ino
+++ arduino/Minitel1B_SSH_Client/Minitel1B_SSH_Client.ino
@@ -46,7 +46,6 @@ const char* ssh_host     = "yourHostIP";   // host ip adress (e.g. "192.168.0.1"
 const char* ssh_username = "yourUsername"; // user name (e.g. "pi")
 const char* ssh_password = "yourPassword"; // user password (e.g. "raspberrypi")
 
-//WiFiClient client
 Minitel minitel(MINITEL_PORT);
 SSHClient sshClient;
 
@@ -81,43 +80,66 @@ void setup() {
   }
   debugPrintf("\n  > WiFi connected with IP %s\n", WiFi.localIP().toString().c_str());
 
-  // We connect to host and open ssh session
-  debugPrintf("\n> SSH session setup\n");
-  debugPrintf("  Connecting to %s as %s\n", ssh_host, ssh_username);
-  if (sshClient.begin(ssh_host, ssh_username, ssh_password) != SSHClient::SSHStatus::OK) {
-    debugPrintf("  > SSH authentication failed\n");
-    while(1) delay(500); // endless
-  }
-  else debugPrintf("  > SSH authentication ok\n");
-  
-  debugPrintf("\n> End of setup\n\n");
-
+  // We launch SSH session in a new task (stack size needs to be larger)
+  debugPrintf("\n> SSH task setup\n");
+  BaseType_t xReturned;
+  xReturned = xTaskCreatePinnedToCore(sshTask, "sshTask", 51200, NULL,
+    (configMAX_PRIORITIES - 1), NULL, ARDUINO_RUNNING_CORE);
+  if (xReturned!=pdPASS) debugPrintf("  > Failed to create task\n");
 }
 
-void loop() {
-
-  // Check ssh channel
-  if (!sshClient.available()) {
-    debugPrintf("ssh channel lost\n");
-    delay(1000);
-    return;
-  }
-
-  // host -> minitel
-  int nbytes = sshClient.receive();
-  if (nbytes > 0) {
-    debugPrintf("[SSH] got %u bytes\n", nbytes);
-    int index = 0;
-    while (index < nbytes) {
-      char b = sshClient.readIndex(index++);
-      minitel.writeByte(b);
+void sshTask(void *pvParameters) {
+  debugPrintf("\n> SSH task running\n");
+  
+  // Open ssh session
+  debugPrintf("  Connecting to %s as %s\n", ssh_host, ssh_username);
+  bool isOpen = sshClient.begin(ssh_host, ssh_username, ssh_password);
+  if (!isOpen) debugPrintf("  > SSH authentication failed\n");
+  
+  // Loop task
+  while (true) {
+    bool cancel = false;
+    // Check ssh channel
+    if (!sshClient.available()) {
+      debugPrintf("ssh channel lost\n");
+      break;
     }
-  }
 
-  // minitel -> host
-  uint32_t key = minitel.getKeyCode(false);
-  if (key != 0) {
+    // host -> minitel
+    int nbytes = sshClient.receive();
+    if (nbytes < 0) {
+      debugPrintf("  > SSH Error while receiving\n");
+      break;
+    }
+    if (nbytes > 0) {
+      debugPrintf("[SSH] got %u bytes\n", nbytes);
+      int index = 0;
+      while (index < nbytes) {
+        char b = sshClient.readIndex(index++);
+        minitel.writeByte(b);
+      }
+    }
+
+    // minitel -> host
+    uint32_t key = minitel.getKeyCode(false);
+    if (key == 0) {
+      vTaskDelay(50/portTICK_PERIOD_MS);
+      continue;
+    }
     debugPrintf("[KB] got code: %X\n", key);
+    switch (key) {
+      // redirect minitel special keys
+      case SOMMAIRE:   key = 0x07;   break; //BEL : ring
+      case GUIDE:      key = 0x07;   break; //BEL : ring
+      case ANNULATION: key = 0x0515; break; //ctrl+E ctrl+U : end of line + remove left
+      case CORRECTION: key = 0x7F;   break; //DEL : delete
+      case RETOUR:     key = 0x01;   break; //ctrl+A : beginning of line
+      case SUITE:      key = 0x05;   break; //ctrl+E : end of line
+      case REPETITION: key = 0x0C;   break; //ctrl+L : clear-screen (current command repeated)
+      case ENVOI:      key = 0x0D;   break; //CR : validate command
+      // intercept ctrl+c
+      case 0x03: cancel = true; break; 
+    }
     // prepare data to send over ssh
     uint8_t payload[4];
     size_t len = 0;
@@ -125,7 +147,36 @@ void loop() {
       payload[3-len] = uint8_t(key);
       key = key >> 8;
     }
-    sshClient.send(payload+4-len, len);
+    if (sshClient.send(payload+4-len, len) < 0) {
+      debugPrintf("  > SSH Error while sending\n");
+      break;
+    }
+    // Intercept CTRL+C:
+    // displaying data received before host get the command can take minutes
+    // we ignore received data to avoid this
+    if (cancel) {
+      debugPrintf(" > Intercepted ctrl+C\n", nbytes);
+      int nbyte = sshClient.flush();
+      minitel.println();minitel.println();
+      minitel.println("\r\r * ctrl+C * ");
+      minitel.println("Warning: received data ignored because of it is too long to display");
+      minitel.print("number of bytes ignored : ");
+      minitel.println(String(nbyte));
+      // send CR to get new input line
+      uint8_t cr = 0x0D;
+      sshClient.send(&cr, 1);
+    }
   }
-  
+  // Closing session
+  debugPrintf(" >  Session closed\n");
+  sshClient.end();
+
+  // Self delete task
+  vTaskDelete(NULL);
+  debugPrintf("\n> SSH task end\n");
+}
+
+void loop() {
+  // Nothing to do here since sshTaskCode has taken over.
+  vTaskDelay(60000 / portTICK_PERIOD_MS);
 }
